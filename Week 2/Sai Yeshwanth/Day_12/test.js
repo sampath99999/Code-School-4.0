e84e80f51ff71291d28646d2ff60a89c05e73fe0
Yashwanth-chary <yeshu98981@gmail.com>
2025-03-02 09:29:00 +0530

    week 2

---
+++ Week 2/Sai Yeshwanth/Day_12/test.js
@@ -0,0 +1,47 @@
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(1, 5); //from-to,accepts negative too
+// console.log(part)
+
+// let str = "Apple, Banana, Kiwi";
+// console.log(str.substring(2, -1)); // any negative index will be considered as 0
+
+// let sub = "Apple, Banana, Kiwi";
+// let s = sub.substr(-4); //start index +/-, but a positive length.
+// console.log(s)
+
+// let text1 = "Hello World!";
+// let text2 = text1.toUpperCase(); // similarly toLowerCase()
+// console.log(text2)
+
+
+// let text1 = "Hello";
+// let text2 = "World";
+// let text3 = text1.concat(" ", text2); //adds two strings
+// console.log(text3)
+
+// let text1 = "      Hello World!      "; //remove extra spaces at both ends
+// let text2 = text1.trim();
+// console.log(text2)                   // also have other methods like trimStart(), trimEnd()
+
+
+// let text = "5";
+// let padded = text.padStart(4, "0"); // adds padding at the start with given character for a given length
+// console.log(padded)  //similarly padEnd()
+
+//toString() converts number to string
+
+//repeat(count) returns the number of times a given string to be repeated
+
+// let text = "Please visit Microsoft and Microsoft!";
+// let newText = text.replace(/Microsoft/g, "W3Schools"); //replaces a string with given string, only first match, case sensitive
+// console.log(newText)  // here a regular expression is used to find all strings matching with Microsoft
+
+//replaceAll(string,replacement)
+
+// let text = "a,b,c,d,e,f";
+// const myArray = text.split(",");  //splits based on the separator provided
+// console.log(myArray) //[ 'a', 'b', 'c', 'd', 'e', 'f' ]
+
+
+
+
